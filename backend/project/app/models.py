7e5ecb0b09cf6de030ba276cad030fe3f1e831aa
Kshitij Nevrekar <71255066+aevnum@users.noreply.github.com>
2024-11-04 15:48:29 +0530

    Removed unnecessary fields

--- backend/project/app/models.py
+++ backend/project/app/models.py
@@ -15,22 +15,6 @@ class User(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     city = models.CharField(max_length=255, blank=True)
     college = models.CharField(max_length=255, blank=True)
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_set',
-        blank=True,
-        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
-        verbose_name='groups'
-    )
-    
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_permissions_set',
-        blank=True,
-        help_text="Specific permissions for this user.",
-        verbose_name='user permissions'
-    )
     
     def __str__(self):
         return self.username
